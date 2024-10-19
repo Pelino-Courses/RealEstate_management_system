292351ee4098f4aac93184691d7a468a660a4c02
= <=>
2024-10-19 20:44:17 +0200

    update system

--- Property_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Property_app.models import Property,Lease,Units,Tenant
-# Register your models here.
-
-admin.site.register(Property)
-admin.site.register(Lease)
-admin.site.register(Units)
-admin.site.register(Tenant)
